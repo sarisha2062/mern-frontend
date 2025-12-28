521115839b1784c255f8ceb5408bc26c2718c329
Sarisha2062 <chaudharysarisha6@gmail.com>
2025-12-28 15:00:30 +0545

    Edit file

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,67 @@ import { useState } from 'react'
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
 import './App.css'
+import { useEffect } from 'react'
+const API_URL = `${import.meta.env.VITE_API_URL}/notes`
+
 
 function App() {
   const [notes, setNotes] = useState([])
   const [editingNote, setEditingNote] = useState(null)
+  const[loading,setLoading]=useState(true)
+  useEffect(() => {
+    fetchNotes()
+  }, [])
 
-  const addNote = (note) => {
-    const newNote = {
-      id: Date.now(),
-      ...note,
-      createdAt: new Date().toISOString()
-    }
-    setNotes([newNote, ...notes])
+  const fetchNotes = async () => {
+      try {
+        const response = await fetch(API_URL)
+        const data = await response.json()
+        setNotes(data)
+      } catch (error) {
+        console.error("Error fetching notes:", error)
+      }
+      finally{
+        setLoading(false)
+      }
   }
 
-  const updateNote = (id, updatedNote) => {
+  const addNote = async (note) =>{
+    try{
+      const response=await fetch(API_URL,{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(note)
+      })
+      const data=await response.json();
+      setNotes([data,...notes])
+    }
+    catch(error){
+      console.error("Error adding note:",error);
+    }
+  }
+
+  const updateNote = async(id, updatedNote) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedNote)
+    })
+    const data = await response.json()    
     setNotes(notes.map(note =>
       note.id === id ? { ...note, ...updatedNote } : note
     ))
     setEditingNote(null)
   }
 
-  const deleteNote = (id) => {
-    setNotes(notes.filter(note => note.id !== id))
-  }
+  const deleteNote = async(id) => {
+    try{
+      await fetch(`${API_URL}/${id}`,{method:"DELETE" })   
+      setNotes(notes.filter(note => note.id !== id))
+    } catch(error){
+      console.error("Error deleting note:",error);
+    } 
+  }     
 
   return (
     <div className="app">
